a1d399d543057099b7416bdd8e808364296a07c4
Andy Obusek <andyo@aweber.com>
2014-01-12 21:03:38 -0500

    Added additional code for Lesson 16

---
+++ 16.py
@@ -0,0 +1,8 @@
+#Code for codecademy.com Lesson 16
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+def print_grades(to_print):
+    for grade in grades:
+        print grade
+print_grades(grades)
+
